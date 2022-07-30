61563aee031b248310928fb31a732f5f05aa85f8
Trishitawhitehat <80470233+Trishitawhitehat@users.noreply.github.com>
2022-07-30 16:31:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var status = "";
+
+function preload() {
+    img = loadImage('bed,pillow.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+}
+
+function back() {
+    window.localStorage("index.html");
+}
